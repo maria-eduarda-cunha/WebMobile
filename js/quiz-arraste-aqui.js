7dd92395e832fde662820a71f64135fd24205d25
GJIto <88221298+GJIto@users.noreply.github.com>
2026-09-22 21:03:31 -0300

    Update quiz-arraste-aqui.js

--- js/quiz-arraste-aqui.js
+++ js/quiz-arraste-aqui.js
@@ -19,9 +19,22 @@ function dragoverHandler(ev) {
     ev.preventDefault();
 }
 
+let resp_corretas = 0;
 function dropHandler(ev) {
     ev.preventDefault();
     const data = ev.dataTransfer.getData("text");
     const texto = document.getElementById(data);
     ev.target.textContent = texto.textContent;
+    
+    const opcao = ev.target;
+    const i = Number.parseInt(opcao.id.split('-')[1])
+    if(opcao.innerText === "Seleção"+i){
+        resp_corretas++;
+        console.log(resp_corretas);
+    }
+    
+    
+    if(resp_corretas === 3){
+        alert('acertou')
+    }
 }
